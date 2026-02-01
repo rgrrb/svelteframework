09abb7ef847e83a2d1e4e53f43f499b4833bed07
rogeriosrib <cjo.rogerribeiro@gmail.com>
2026-02-01 18:52:32 -0300

    fix: arrumando estrutura

---
+++ src/components/input.svelte
@@ -0,0 +1,18 @@
+
+<input class="search-bar" type="text" placeholder="Pesquise uma raça de cachorro">
+
+<style>
+    .search-bar{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-top: 2dvh;
+        width: 40rem;
+        border-radius: 20px;
+        padding: 10px;
+        padding-left: 20px;
+        height: 40px;
+        box-shadow: 1px 10px 47px 0px rgba(0,0,0,0.10);
+
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+    import Input from "../components/input.svelte";
+
+    const raca = "bulldog"
+
+    import { onMount } from "svelte";
+
+    import doogleImg from "../public/Doogle.png"
+
+    const endpoint = `https://dog.ceo/api/breed/${raca}/images`;
+
+    onMount(async function () {
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        console.log(data);
+    });
+
+    
+</script>
+
+<main>
+    <img src={doogleImg} alt="Doogle"/>
+    <Input />
+</main>
+
+<style>
+    main {
+        background-color: f0f0f0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        width: 100%;
+        gap: 20px;
+    }
+    img {
+        height: fit-content;
+    }
+</style>
